0044f2ab9bbd0ad4c65219b47d5b0d7b969677c4
leo <62425425+lleaob@users.noreply.github.com>
2026-04-14 11:40:30 +0100

    feat: Add Rover class with Rotate method and initial unit test.

---
+++ MarsRover/MarsRover.Console/LogicLayer/Rover.cs
@@ -0,0 +1,36 @@
+﻿using MarsRover.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.ConsoleApp.LogicLayer
+{
+    public class Rover(int x, int y, CompassDirection facing)
+    {
+        public int X = x;
+        public int Y = y;
+        public CompassDirection Facing = facing;
+
+        //Rotate takes current direction and instruction and modifies direction
+        public CompassDirection Rotate(CompassDirection currentDirection, Instruction instuction)
+        {
+            switch (instuction)
+            {
+                case Instruction.L:
+
+                break;
+
+                case Instruction.R:
+
+                break; 
+            }
+
+            return currentDirection;
+                
+        }
+
+
+    }
+}
---
+++ MarsRover/MarsRover.Tests/RoverTests.cs
@@ -0,0 +1,20 @@
+﻿using MarsRover.ConsoleApp.LogicLayer;
+using MarsRover.ConsoleApp.Models;
+using Shouldly;
+namespace MarsRover.Tests;
+
+public class RoverTests
+{
+    Rover defaultRover = new Rover(0,0,CompassDirection.N);
+
+    [Test]
+    public void Rotate_ReturnsN_MInput()
+    {
+        CompassDirection expected = CompassDirection.N;
+
+        var result = defaultRover.Rotate(CompassDirection.N, Instruction.M);
+
+        result.ShouldBe(expected);
+
+    }
+}
